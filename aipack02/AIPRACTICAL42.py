83dd1575aa079cb6e9d7e7fccda2868dc55f9f26
Suhail Khan <suhailkhan2k5p@email.com>
2025-07-17 10:46:37 +0530

    Resolved conflict and saved local changes

--- aipack02/AIPRACTICAL42.py
+++ aipack02/AIPRACTICAL42.py
@@ -5,6 +5,6 @@ arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 
 # Reshaping the array into a 2D array
 # '-1' allows to calculate the number of rows based on the total number of elements
-reshaped_arr = np.reshape(arr, (4, -1))
+reshaped_arr = np.reshape(arr, (-1,3,4))
 
 print(reshaped_arr)
