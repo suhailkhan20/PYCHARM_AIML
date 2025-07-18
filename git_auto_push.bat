f5a410d83d36ed6fb328b0bcf1be692f5e77c72a
Suhail Khan <suhailkhan2k5p@email.com>
2025-07-18 10:09:33 +0530

    Auto commit on 18-07-2025 10:09:33.21

--- git_auto_push.bat
+++ git_auto_push.bat
@@ -1,8 +1,14 @@
 @echo off
 cd /d "%~dp0"
-git checkout main
+
+REM Check if there are changes
+git diff --quiet && git diff --cached --quiet
+IF ERRORLEVEL 1 (
+    git add .
+    git commit -m "Auto commit on %date% %time%"
+)
+
 git pull --rebase
-git add .
-git commit -m "Auto commit on %date% %time%"
 git push
+
 pause
