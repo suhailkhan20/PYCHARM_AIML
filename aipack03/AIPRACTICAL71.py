ed8adf7b2545ccdfda0251590ad3e0fee3dccc22
Suhail Khan <suhailkhan2k5p@email.com>
2025-07-22 10:10:18 +0530

    Auto commit on 22-07-2025 10:10:18.48

--- aipack03/AIPRACTICAL71.py
+++ aipack03/AIPRACTICAL71.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+plt.plot([1, 2, 3, 4], [1, 4, 12, 5])
 
 plt.xlabel("-------Some Numbers------->")
 plt.ylabel("-------Squared Value------->")
