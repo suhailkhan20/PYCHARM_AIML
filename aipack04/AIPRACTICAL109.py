23c0e409c21bcd0522f262ac801ba207b53f344c
Suhail Khan <suhailkhan2k5p@email.com>
2025-07-30 09:45:32 +0530

    Auto commit on 30-07-2025  9:45:32.81

--- aipack04/AIPRACTICAL109.py
+++ aipack04/AIPRACTICAL109.py
@@ -7,11 +7,19 @@ df = pd.DataFrame(np.random.randn(6,4),
                   index = pd.date_range('20190101', periods=6, freq='D'),
                   columns = ['A','B','C','D'] )
 
-print("Original \n",df)
+
+print("Only one column access: ")
+print(df.A)
+print("\n\n")
+print(df['B'])
+print("\n\n")
+print(df['D'])
 
 print("\n\n")
-
-print(df.sort_index(axis=1, ascending=False))
+print(df['2019-01-01' : '2019-01-03'])
 
 print("\n\n")
-print(df.sort_index(axis=0, ascending=False))
+print(df[0:3])
+
+print("\n\n")
+print(df['20190102':'20190104'])
